b59d21828c2eaf28048a482d38bd4e0e92ae296a
firelemons <shenyang64@gmail.com>
2024-01-02 20:22:16 -0600

    fix typescript warnings

--- src/typeChecker.ts
+++ src/typeChecker.ts
@@ -3,37 +3,44 @@ enum Type {
   array = 'array'
 }
 
+interface objectWithKeys {
+  [key: string]: any
+}
+
+function isObject(obj: any):boolean {
+  return typeof obj === 'object' && !Array.isArray(obj) && obj !== null
+}
+
+function isString(obj: any): obj is string {
+  return typeof obj === 'string'
+}
+
+function validateObjectMember(obj: objectWithKeys, key: string, type: Type): void {
+  if (!(key in obj)){
+    throw new ReferenceError(`key "${key}" was not found in the object`)
+  }
+
+  const member = obj[key]
+
+  switch (type) {
+    case Type.array:
+      if(!(Array.isArray(member))) {
+        throw new TypeError(`Member "${key}" was found not to be an array`)
+      }
+
+      break;
+    case Type.string:
+      if (isString(member)) {
+        throw new TypeError(`Member "${key}" was found not to be an string`)
+      }
+
+      break;
+  }
+}
+
 module.exports = {
-  isObject: function (obj: any):boolean {
-    return typeof obj === 'object' && !Array.isArray(obj) && obj !== null
-  },
-
-  isString: (obj: any): obj is string => {
-    return typeof obj === 'string'
-  },
-
-  validateObjectMember: (obj: object, key: string, type: Type): void => {
-    if (!(key in obj)){
-      throw new ReferenceError(`key "${key}" was not found in the object`)
-    }
-
-    const member = obj[key]
-
-    switch (type) {
-      case Type.array:
-        if(!(Array.isArray(member))) {
-          throw new TypeError(`Member "${key}" was found not to be an array`)
-        }
-
-        break;
-      case Type.string:
-        if (!this.isString(member)) {
-          throw new TypeError(`Member "${key}" was found not to be an string`)
-        }
-
-        break; 
-    }
-  },
-
+  isObject: isObject,
+  isString: isString,
+  validateObjectMember: validateObjectMember,
   types: Type
 }
