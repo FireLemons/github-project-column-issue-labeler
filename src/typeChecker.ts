5da51a1c42871c3ba92ad075db83fafa2df5643f
firelemons <shenyang64@gmail.com>
2024-01-16 07:36:32 -0600

    minor spelling error

--- src/typeChecker.ts
+++ src/typeChecker.ts
@@ -31,7 +31,7 @@ function validateObjectMember(obj: objectWithKeys, key: string, type: Type): voi
       break;
     case Type.string:
       if (isString(member)) {
-        throw new TypeError(`Member "${key}" was found not to be an string`)
+        throw new TypeError(`Member "${key}" was found not to be a string`)
       }
 
       break;
