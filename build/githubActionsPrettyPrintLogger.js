50bf6f4c650bd4a8a587c0cad0772037c8c46345
firelemons <shenyang64@gmail.com>
2024-02-12 19:53:43 -0600

    removed github actions calls from logger

--- build/githubActionsPrettyPrintLogger.js
+++ build/githubActionsPrettyPrintLogger.js
@@ -1,34 +1,10 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.warn = exports.error = exports.info = void 0;
 const cli_color_1 = __importDefault(require("cli-color"));
-const core = __importStar(require("@actions/core"));
 function makePrettyString(message, indentation, applyColor) {
     const messageLines = message.split('\n');
     return messageLines.map((line) => {
@@ -36,16 +12,14 @@ function makePrettyString(message, indentation, applyColor) {
     }).join('\n');
 }
 function info(message, indentation = '') {
-    const adjustedIndentation = indentation + '         '; // Used to line up with warning messages prefixed with "Warning: "
-    core.info(makePrettyString(message, adjustedIndentation, cli_color_1.default.cyan));
+    console.info(makePrettyString('INFO:' + message, indentation, cli_color_1.default.cyan));
 }
 exports.info = info;
 function error(message, indentation = '') {
-    const adjustedIndentation = indentation + '  ';
-    core.error(makePrettyString(message, adjustedIndentation, cli_color_1.default.red));
+    console.error(makePrettyString('FAIL' + message, indentation, cli_color_1.default.red));
 }
 exports.error = error;
 function warn(message, indentation = '') {
-    core.warning(makePrettyString(message, indentation, cli_color_1.default.yellow));
+    console.warn(makePrettyString('WARN' + message, indentation, cli_color_1.default.yellow));
 }
 exports.warn = warn;
--- src/githubActionsPrettyPrintLogger.ts
+++ src/githubActionsPrettyPrintLogger.ts
@@ -1,5 +1,4 @@
 import commandLineColor from 'cli-color'
-import * as core from '@actions/core'
 
 function makePrettyString (message: string, indentation: string, applyColor: (...text: any[]) => string): string {
   const messageLines = message.split('\n')
@@ -10,16 +9,13 @@ function makePrettyString (message: string, indentation: string, applyColor: (..
 }
 
 export function info (message: string, indentation: string = '') {
-  const adjustedIndentation = indentation + '         ' // Used to line up with warning messages prefixed with "Warning: "
-
-  core.info(makePrettyString(message, adjustedIndentation, commandLineColor.cyan))
+  console.info(makePrettyString('INFO:' + message, indentation, commandLineColor.cyan))
 }
 
 export function error (message: string, indentation: string = '') {
-  const adjustedIndentation = indentation + '  '
-  core.error(makePrettyString(message, adjustedIndentation, commandLineColor.red))
+  console.error(makePrettyString('FAIL' + message, indentation, commandLineColor.red))
 }
 
 export function warn (message: string, indentation: string = '') {
-  core.warning(makePrettyString(message, indentation, commandLineColor.yellow))
+  console.warn(makePrettyString('WARN' + message, indentation, commandLineColor.yellow))
 }
